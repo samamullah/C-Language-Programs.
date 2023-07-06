84668cd3f807f685947cb7830a2302379af1cbdc
samamullah <108141059+samamullah@users.noreply.github.com>
2023-07-06 23:43:18 +0500

    Add files via upload

---
+++ Sum Two  Number using 2D Array/Sum Two  Number using 2D Array/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int Array_1[2][2];
+    int Array_2[2][2];
+    int sum[2][2];
+    int i;
+    int j;
+    
+    printf("Enter value of 1st matric  : \n");
+    for (i=0; i<2; i++)
+    {
+        for (j=0; j<2; j++)
+        {
+            scanf("%d",& Array_1[i][j]);
+        }
+    }
+    
+    printf("Enter value of 2nd matric  : \n");
+    for (i=0; i<2; i++)
+    {
+        for (j=0; j<2; j++)
+        {
+            scanf("%d",& Array_2[i][j]);
+        }
+    }
+    printf("The sum of two matric : \n");
+    for (i=0; i<2; i++)
+    {
+        for (j=0; j<2; j++)
+        {
+            sum[i][j]=Array_1[i][j]+Array_2[i][j];
+            printf("%d\t",sum[i][j]);
+        }
+        printf("\n");
+       
+}
+     return 0;
+}
